101c2a22888341fc2332baf1d95cb4ad1e2b437e
Ruan S <76751124+isruan@users.noreply.github.com>
2022-07-09 09:26:50 -0400

    Create Index.py

---
+++ Index.py
@@ -0,0 +1,11 @@
+#print("Welcome to the Band Name Generator.")
+#street = input("What's name of the city you grew up in?\n")
+#pet = input("What's your pet's name?\n")
+#print("Your band name could be " + street + " " + pet)
+print("welcome to the band name generator")
+
+city = input("what is name of the city you grew in?\n")
+
+pet = input("what is the name for a cool pet?\n")
+
+print("the name of your band could be\n" + city + " " + pet)
